d3c5b11224dba4cf07f446b55fa69c5ae1478af1
syed.musfar <syed.musfer@convexinteractive.com>
2024-01-11 15:21:41 +0500

    about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,6 +5,22 @@ const About = () => {
     <div style={{marginTop:'90px'}} >
         <div className='scndsec' style={{height:'600px'}} >
 
+            <div className='uk-container   ' style={{backgroundColor:'white' , height:'800px'}}  >
+
+                <div className=' uk-grid uk-child-width-1-2@s'  style={{marginTop:'70px'}}>
+                  <div>
+                <h1>About Us</h1>
+                <h5>Your go-to solution for custom software development We are a software-building company with a diverse team of developers, designers, technologists, and strategists who share a passion for collaboration and success. Our unique software developers build remarkable software solutions tailored to your specified requirements</h5>
+                </div>
+                <div>
+                    <img src='images/logo.jpeg' />
+                </div>
+
+                </div>
+
+                
+            </div>
+
         </div>
 
 
